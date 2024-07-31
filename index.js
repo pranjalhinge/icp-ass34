369854653f8ea8c3bc584e424d64edf3f4ea4b45
pranjalhinge <pranjalhinge4@gmail.com>
2024-07-31 23:52:16 +0530

    find largest no in arry

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ const students =
 for(const stud of students){
     console.log(`Hello ${stud}`)
 }
+
+const marks = [7,100,729,21,49]
+let largest = marks[0]
+
+for(const num of marks){
+    if (num>largest){
+        largest = num
+    }
+}
+console.log(largest)
